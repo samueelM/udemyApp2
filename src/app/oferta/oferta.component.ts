0ddadb6319b2911af9e26beec1768bfd7a857f66
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-09-23 08:58:55 -0300

    Aula 144

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
-import { Observable, interval, Observer } from 'rxjs';
+import { Observable, interval, Observer, observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -10,14 +10,23 @@ import { Observable, interval, Observer } from 'rxjs';
   styleUrls: ['./oferta.component.css'],
   providers: [ OfertasService ]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit/*, OnDestroy*/ {
 
   public oferta: Oferta;
+  // private tenmpoObservableSubscription: Subscription;
+  // private meuObservableTesteSubscription: Subscription;
+
 
   constructor(
     private route: ActivatedRoute,
     private ofertasService: OfertasService
   ) { }
+
+  // ngOnDestroy(): void {
+  //   this.tenmpoObservableSubscription.unsubscribe();
+  //   this.meuObservableTesteSubscription.unsubscribe();
+  // }
+
 
   ngOnInit() {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
@@ -30,21 +39,26 @@ export class OfertaComponent implements OnInit {
       (erro: any) => console.log (erro),
       () => 'processamento foi classificado como concluído!'
       ); */
-      
-      /*
-      let tempo = interval(3000);
 
-      tempo.subscribe ((intervalo: number) => console.log (intervalo));
-      */
 
-      // observable
-      let meuObservableTeste = Observable.create ((observer: Observer<number>) => {
-          observer.next(1);
-          observer.next(3);
-      });
+      // let tempo = interval(2000);
 
-      // observador
-      meuObservableTeste.subscribe ((resultado: number) => console.log (resultado + 10));
+      // this.tenmpoObservableSubscription = tempo.subscribe ((intervalo: number) => console.log (intervalo));
+
+
+      // // observable
+      // let meuObservableTeste = Observable.create ((observer: Observer<number>) => {
+      //     observer.next(1);
+      //     observer.next(3);
+      //     // observer.error ('algum erro foi encontrado na stream de eventos...');
+      //     observer.complete ();
+      // });
+
+      // // observador
+      // this.meuObservableTesteSubscription = meuObservableTeste.subscribe (
+      //   (resultado: number) => console.log (resultado + 10),
+      //   (erro: string) => console.log (erro),
+      //   () => console.log ('a stream de eventos foi finalizada'));
 
 
   }
