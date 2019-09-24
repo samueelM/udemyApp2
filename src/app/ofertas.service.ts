1f1c51fa6593eab7a3ba228ef2a5e2a4968884e5
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-09-24 13:38:58 -0300

    Aula 148

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -52,9 +52,11 @@ export class OfertasService {
     }
 
     public pesquisaOfertas (termo: string): Observable<Oferta[]> {
-      return this.http.get (`${URL_API}/ofertas?descricao_oferta=${termo}`).
+      return this.http.get (`${URL_API}/ofertas?descricao_oferta_like=${termo}`).
         pipe(
-            map ((resposta: any) => resposta.json));
+            map ((resposta: any) => {
+              return resposta;
+            }));
     }
 
 }
