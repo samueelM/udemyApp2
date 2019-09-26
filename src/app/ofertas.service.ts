282c9d2d0bd3e72bd04a73b0b16958590a91ac8b
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-09-26 13:34:13 -0300

    Aula 153

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,61 +5,56 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { retry } from 'rxjs/operators';
 import { Http, Response } from '@angular/http';
-import { HttpModule } from '@angular/http';
+
 
 
 @Injectable()
 export class OfertasService {
 
+    // tslint:disable-next-line: deprecation
     constructor(private http: Http) {}
 
     public getOfertas(): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
-            .then((resposta: Response) => {
-                return resposta.json();
-            });
+            // tslint:disable-next-line: deprecation
+            .then((resposta: Response) => resposta.json());
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
-            .then((resposta: Response) => {
-                return resposta.json();
-            });
+            // tslint:disable-next-line: deprecation
+            .then((resposta: Response) => resposta.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
         return this.http.get(`${URL_API}/ofertas?id=${id}`)
             .toPromise()
-            .then((resposta: Response) => {
-                return resposta.json()[0];
-            });
+            // tslint:disable-next-line: deprecation
+            .then((resposta: Response) => resposta.json()[0]);
     }
 
     public getComoUsarOfertaPorID (id: number): Promise<string> {
         return this.http.get (`${URL_API}/como-usar?id=${id}`).
             toPromise()
-            .then ((resposta: Response) => {
-                return resposta.json()[0].descricao;
-            });
+            // tslint:disable-next-line: deprecation
+            .then ((resposta: Response) => resposta.json()[0].descricao);
     }
 
     public getOndeFicaOfertaPorID (id: number): Promise<string> {
         return this.http.get (`${URL_API}/onde-fica?id=${id}`).
             toPromise()
-            .then ((resposta: Response) => {
-                return resposta.json()[0].descricao;
-            });
+            // tslint:disable-next-line: deprecation
+            .then ((resposta: Response) => resposta.json()[0].descricao);
     }
 
     public pesquisaOfertas (termo: string): Observable<Oferta[]> {
       return this.http.get (`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
         .pipe(
             retry(10),
-            map ((resposta: Response) => {
-              return resposta.json();
-            }));
+            // tslint:disable-next-line: deprecation
+            map ((resposta: Response) => resposta.json()));
     }
 
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,9 @@
 import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import { switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-topo',
@@ -12,19 +14,36 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>;
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    // const obs: Observable<string> = this.subjectPesquisa.asObservable();
+    // obs.pipe (switchMap((termo: string) => {
+    //   console.log ('requisicao http: ');
+    //   return this.ofertasService.pesquisaOfertas (termo);
+    // }));
+
+    const obs: Observable<any> = this.subjectPesquisa.asObservable();
+
+    this.ofertas = obs.pipe (switchMap((termo: string) => {
+      console.log ('requisicao http: ');
+      return this.ofertasService.pesquisaOfertas (termo);
+    }));
+
+    this.ofertas.subscribe ((ofertas: Oferta[]) => console.log (ofertas));
   }
 
   public pesquisa (termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas (termoDaBusca);
+    console.log ('termo da busca: ' + termoDaBusca);
+    this.subjectPesquisa.next (termoDaBusca);
+    // this.ofertas = this.ofertasService.pesquisaOfertas (termoDaBusca);
 
-    this.ofertas.subscribe(
-             (ofertas: Oferta[]) => console.log (ofertas),
-             (erro: any) => console.log ('Erro status:', erro.status),
-             () => console.log ('Fluxo de eventos finalizado..'));
+    // this.ofertas.subscribe(
+    //          (ofertas: Oferta[]) => console.log (ofertas),
+    //          (erro: any) => console.log ('Erro status:', erro.status),
+    //          () => console.log ('Fluxo de eventos finalizado..'));
   }
 
 }
