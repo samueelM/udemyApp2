d2f07072e9b31cfc45ca570f5d51c8eae53fb5b9
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-10-03 13:54:55 -0300

    Aula 178

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,15 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public endereco = 'Rua XYZ';
-  public numero: number = 10;
+  public endereco = '';
+  public numero: string = '';
   public complemento = '';
-  public formaPagamento = 'debito';
+  public formaPagamento = '';
 
+  // controle de validação dos campos
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  atualizaEndereco (endereco: string): void {
+    this.endereco = endereco;
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
+  }
+
+  atualizaNumero (numero: string): void {
+    this.numero = numero;
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
+  }
+
+  atualizaComplemento (complemento: string): void {
+    this.complemento = complemento;
+
+    if (this.complemento.length > 0) {
+      this.complementoValido = true;
+    }
+  }
+
+  atualizaFormaPagamento (formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
+
+    if (this.formaPagamento !== '') {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
+
+
+  }
 }
