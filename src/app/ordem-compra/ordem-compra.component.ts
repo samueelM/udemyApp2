092eebf47730c977514e1597e2d413c65419ff78
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-10-08 09:59:49 -0300

    Aula 211

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -10,12 +11,38 @@ import { Pedido } from '../shared/pedido.model'
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number;
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null,  [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl(null, [Validators.required])
+  });
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
+
   ngOnInit() {
-    
+
   }
 
   public confirmarCompra(): void {
+    if (this.formulario.status === 'INVALID') {
+      this.formulario.get('endereco').markAsTouched();
+      this.formulario.get('numero').markAsTouched();
+      this.formulario.get('complemento').markAsTouched();
+      this.formulario.get('formaPagamento').markAsTouched();
+    } else {
+      const pedido = new Pedido(this.formulario.value.endereco,
+                                this.formulario.value.numero,
+                                this.formulario.value.complemento,
+                                this.formulario.value.formaPagamento);
+      console.log (pedido);
+
+      this.ordemCompraService.efetivarCompra(pedido).subscribe ((idPedido: number) => {
+        this.idPedidoCompra = idPedido;
+      });
+    }
+
   }
 }
