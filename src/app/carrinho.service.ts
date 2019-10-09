2a3ff140193e7b3110b354b0f4198f81ff1cf8d7
Samuel M. de Oliveira <samuel.oliveira@serpro.gov.br>
2019-10-09 13:56:29 -0300

    Aula 214

---
+++ src/app/carrinho.service.ts
@@ -0,0 +1,13 @@
+import { ItemCarrinho } from './shared/item-carrinho.model';
+
+class CarrinhoService {
+
+  public itens: ItemCarrinho[] = [];
+
+  public exibirItens (): ItemCarrinho[] {
+    return this.itens;
+  }
+
+}
+
+export default CarrinhoService;
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import CarrinhoService from '../carrinho.service';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+  providers: [ OrdemCompraService, CarrinhoService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -19,11 +20,14 @@ export class OrdemCompraComponent implements OnInit {
     'formaPagamento': new FormControl(null, [Validators.required])
   });
 
-  constructor(private ordemCompraService: OrdemCompraService) { }
+  constructor(
+              private ordemCompraService: OrdemCompraService,
+              private carrinhoService: CarrinhoService,
+              ) { }
 
 
   ngOnInit() {
-
+    console.log ('Arrays de itens do carrinho: ' + this.carrinhoService.exibirItens());
   }
 
   public confirmarCompra(): void {
